1fd0445ad54b561e983f2993f566fcd7f6157ed7
megadeth10 <1metallica@hanmail.net>
2018-05-18 16:20:34 +0900

    미사용 component 삭제

--- src/components/DisplayPannel.js
+++ src/components/DisplayPannel.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Person from './Person'
 import {warpperContext} from '../context/MyContext'
 
 class DisplayPannel extends React.Component {
